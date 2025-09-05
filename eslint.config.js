1c8a96e8073df65a89d19e19c2f23786445ed6bc
Mike Fretwell <mfretwell@mGeo.ca>
2025-09-05 09:52:52 -0600

    Lint catcher pt 1

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,55 @@
-// eslint.config.js
-import js from '@eslint/js';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import tseslint from 'typescript-eslint';
 import reactPlugin from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
 
 export default [
-  js.configs.recommended,
+  {
+    ignores: [
+      '**/node_modules/**', 
+      '**/dist/**', 
+      '**/build/**',
+      '.eslintrc.cjs',
+      'app/dev-dist/**', // Ignore service worker files completely since they're generated
+      // Add any other patterns that were in your .eslintignore file
+    ]
+  },
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      parser: tsParser,
+      parser: tseslint.parser,
       parserOptions: {
-        project: './tsconfig.json',
-        ecmaFeatures: { jsx: true },
-      },
+        project: './tsconfig.eslint.json',
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
     },
     plugins: {
-      '@typescript-eslint': tseslint,
-      react: reactPlugin,
-      'react-hooks': reactHooks,
+      '@typescript-eslint': tseslint.plugin,
+      'react': reactPlugin,
+      'react-hooks': reactHooksPlugin
     },
     rules: {
-      // Add your rules here
-    },
+      // We can see from the available rules that these exist
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-require-imports': 'warn',
+      
+      // Add other TypeScript rules as needed
+      
+      // React rules
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
+    }
   },
+  // Special config for JavaScript files
+  {
+    files: ['**/*.js'],
+    rules: {
+      // Disable TypeScript-specific rules for JS files
+      '@typescript-eslint/no-require-imports': 'off'
+    }
+  }
 ];
